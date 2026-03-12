343ae8ee155ab478ee3d02f7a05480231936e179
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-12 16:53:05 +0800

    typed the conditions

--- getting_2_numbers_and_print_not_equal_if_not_same_value.py
+++ getting_2_numbers_and_print_not_equal_if_not_same_value.py
@@ -2,4 +2,8 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 #compare 2 numbers and decide if not equal
+if num1 != num2:
+    print("Not Equal")
 #add else input
+else :
+    print("Equal")
