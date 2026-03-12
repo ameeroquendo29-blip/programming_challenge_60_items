12ee63382844a3706e6a5a1ca0ad02ed12c563ca
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-12 13:44:55 +0800

    added another statement because i forgot that even numbers only

--- printing_all_even_numbers_from_0_to_100.py
+++ printing_all_even_numbers_from_0_to_100.py
@@ -3,5 +3,6 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter last number: "))
 #for loop condition
 for num in range(num1, num2 + 1):
-    print(num)
+    if num % 2 == 0:
+        print(num)
 #print
