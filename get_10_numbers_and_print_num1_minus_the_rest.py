bd15e387073b1ecb0a104f496dbf52aaa78aade7
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-19 01:23:45 +0800

    typed the inputs

--- get_10_numbers_and_print_num1_minus_the_rest.py
+++ get_10_numbers_and_print_num1_minus_the_rest.py
@@ -1,2 +1,12 @@
 #input
+num = float(input("Enter first number: "))
+num -= float(input("Enter second number: "))
+num -= float(input("Enter third number: "))
+num -= float(input("Enter fourth number: "))
+num -= float(input("Enter fifth number: "))
+num -= float(input("Enter sixth number: "))
+num -= float(input("Enter seventh number: "))
+num -= float(input("Enter eighth number: "))
+num -= float(input("Enter ninth number: "))
+num -= float(input("Enter tenth number: "))
 #operations and print
