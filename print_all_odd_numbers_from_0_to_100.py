f516732e66e13647f4c9580ea44478c391ee5aec
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-19 02:46:31 +0800

    changed the for loop and print

--- print_all_odd_numbers_from_0_to_100.py
+++ print_all_odd_numbers_from_0_to_100.py
@@ -3,5 +3,6 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter last number: "))
 #for loop condition
 for num in range(num1, num2 + 1):
-    if num % 2 == 0:
+    if num % 2 == 1:
+        print(num)
 #print
