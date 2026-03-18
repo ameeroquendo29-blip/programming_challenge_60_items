b28e8fdd0c58465c4e6e22633afe509e2925eb85
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-19 03:05:10 +0800

    typed the print

--- print_all_except_those_who_end_with_0_or_5.py
+++ print_all_except_those_who_end_with_0_or_5.py
@@ -2,4 +2,7 @@
 num1 = int(input('Enter first number: '))
 num2 = int(input('Enter second number: '))
 #for loop statement
+for num in range(num1, num2 + 1):
+    if num % 5 != 0:
+        print(num)
 #conditions
