7565e565526d34b1526b87f400598351d8b02b14
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-20 01:08:25 +0800

    typed the for loop and if condition

--- get_10_numbers_and_display_numbers_that does_not_have_duplicate.py
+++ get_10_numbers_and_display_numbers_that does_not_have_duplicate.py
@@ -1,4 +1,10 @@
 #input
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+for num in numbers:
+    if numbers.count(num) == 1:
+        print(num, end=',')
 #for loop
 #if condition
 #counter and print
