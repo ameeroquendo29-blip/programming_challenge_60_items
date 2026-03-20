21bb51c8e5864ec5054851dd12b30927ce7829fa
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-20 20:09:27 +0800

    while loop

--- input_number_and_display_average_until_invalid.py
+++ input_number_and_display_average_until_invalid.py
@@ -4,14 +4,15 @@
 #while loop
 numbers = set()
 count = 0
-num = input("Enter a number: ")
-numbers.add(num)
-count += 1
-if not num.isdigit():
-    print("Invalid input")
-if numbers:
-    average = sum(numbers) / count
-    print("The average is", average)
+while True:
+    num = input("Enter a number: ")
+    numbers.add(int(num))
+    count += 1
+    if not num.isdigit():
+        print("Invalid input")
+    if numbers:
+        average = sum(numbers) / count
+        print("The average is", average)
 
 
 
