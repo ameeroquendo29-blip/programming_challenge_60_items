5e95681bc2a6ca550a7d306a49e56461ee328961
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-20 17:25:41 +0800

    if statement and storage then print

--- input_10_numbers_and_display_all_with_duplicate.py
+++ input_10_numbers_and_display_all_with_duplicate.py
@@ -2,5 +2,12 @@
 #for loop
 #if conditions
 #counter and print
+numbers = []
+duplicates = []
 for i in range(10):
     num = int(input("Enter a number: "))
+    if num in numbers:
+        duplicates.append(num)
+    else:
+        numbers.append(num)
+print(duplicates)
