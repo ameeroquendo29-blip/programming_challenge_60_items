a8d449f1935fdbe442e493e4383babbdfe365737
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-20 20:07:09 +0800

    conditions

--- input_number_and_display_average_until_invalid.py
+++ input_number_and_display_average_until_invalid.py
@@ -7,6 +7,11 @@ count = 0
 num = input("Enter a number: ")
 numbers.add(num)
 count += 1
+if not num.isdigit():
+    print("Invalid input")
+if numbers:
+    average = sum(numbers) / count
+    print("The average is", average)
 
 
 
