f4cc0f8f179f5bb527ccfe21e2e5fa9c7ee06653
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-20 19:28:02 +0800

    another if function and added loop

--- input_a_number_and_display_the_number_with_highest_duplicated.py
+++ input_a_number_and_display_the_number_with_highest_duplicated.py
@@ -3,10 +3,14 @@
 #store
 #loop
 numbers = []
-num = int(input("Enter a number: "))
-if not num.isdigit():
-    print("Invalid input")
-    break
-numbers.append(num)
+while True:
+    num = input("Enter a number: ")
+    if not num.isdigit():
+        print("Invalid input")
+        break
+    numbers.append(int(num))
+if numbers:
+    highest_duplicate = max(set(numbers), key=numbers.count)
+    print(highest_duplicate)
 
 
