c88b9d217afcc421c93b9133a7277db9a726df40
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-21 02:13:39 +0800

    printing in pascal casing

--- pascal_casing.py
+++ pascal_casing.py
@@ -2,3 +2,8 @@
 #change casing
 #print
 cutiepie = input("Enter a word or sentence: ")
+words = cutiepie.split()
+pascal_case = ""
+for word in words:
+    pascal_case += word.capitalize()
+print(pascal_case)
