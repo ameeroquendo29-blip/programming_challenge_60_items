17622dee173df38ea66d02e18ed0c3b0acd2d9b1
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-20 15:57:36 +0800

    changed the list into set to remove duplicates

--- get_10_numbers_and_display_numbers_but_no_duplicates.py
+++ get_10_numbers_and_display_numbers_but_no_duplicates.py
@@ -1,8 +1,9 @@
 #input
 numbers = []
 for i in range(10):
-    num = float(input("Enter a number: "))
+    num = int(input("Enter a number: "))
     numbers.append(num)
+(print(numbers))
 #for loop condition
 #store
 #print
