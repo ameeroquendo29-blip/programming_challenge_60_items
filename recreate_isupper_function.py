c87533534836c7253e3822c35e6ab2d10b76b827
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-29 23:42:43 +0800

    if and else functions then print

--- recreate_isupper_function.py
+++ recreate_isupper_function.py
@@ -2,3 +2,7 @@
 #if statements and functions
 #print
 cutiepie = input("Enter words or sentence: ").swapcase()
+if cutiepie.islower():
+    print("The characters are all uppercase")
+else:
+    print("The characters are not all uppercase")
