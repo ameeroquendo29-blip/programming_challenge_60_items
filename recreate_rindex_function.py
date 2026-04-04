8dd253da64df32d929aacf6f3592538139414e43
AmeerOquendo <ameeroquendo29@gmail.com>
2026-04-05 00:54:34 +0800

    raising error

--- recreate_rindex_function.py
+++ recreate_rindex_function.py
@@ -7,3 +7,4 @@ def manual_rindex(text, sub):
     for i in range(len(text) - sub_len, -1, -1):
         if text[i : i + sub_len] == sub:
             return i
+    raise ValueError(f"substring '{sub}' not found")
