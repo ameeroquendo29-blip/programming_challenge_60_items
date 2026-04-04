23fd4a8da8b5031342bf923d1a6477eb4fe3a8db
AmeerOquendo <ameeroquendo29@gmail.com>
2026-04-05 00:53:42 +0800

    for loop and if statements

--- recreate_rindex_function.py
+++ recreate_rindex_function.py
@@ -2,3 +2,8 @@
 #if statement
 #errors
 #print
+def manual_rindex(text, sub):
+    sub_len = len(sub)
+    for i in range(len(text) - sub_len, -1, -1):
+        if text[i : i + sub_len] == sub:
+            return i
