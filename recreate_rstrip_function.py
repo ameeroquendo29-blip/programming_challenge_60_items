d3d1b2fafd080252e00d841690724e25583abb65
AmeerOquendo <ameeroquendo29@gmail.com>
2026-04-05 00:20:33 +0800

    finished the code

--- recreate_rstrip_function.py
+++ recreate_rstrip_function.py
@@ -1,3 +1,10 @@
 #input
 #while function
 #print
+text = input("Enter a string: ")
+def manual_rstrip(text, chars_to_remove=" "):
+    i = len(text) - 1
+    while i >= 0 and text[i] in chars_to_remove:
+        i -= 1
+    return text[:i + 1]
+print(f"{manual_rstrip(text , '. ')}")
