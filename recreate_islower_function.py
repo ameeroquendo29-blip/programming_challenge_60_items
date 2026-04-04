8bd2271044ef08b4210b46a571b8d6cfbb34758e
AmeerOquendo <ameeroquendo29@gmail.com>
2026-04-05 00:34:33 +0800

    if statements and print

--- recreate_islower_function.py
+++ recreate_islower_function.py
@@ -2,3 +2,7 @@
 #if statements and functions
 #print
 cutiepie = input("Enter words or sentence: ").swapcase()
+if cutiepie.isupper():
+    print("The characters are all lowercase")
+else:
+    print("The characters are not all lowercase")
