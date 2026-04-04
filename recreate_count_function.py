3f3150870389a9add300749acf4bc7c90c27ab90
AmeerOquendo <ameeroquendo29@gmail.com>
2026-04-05 00:45:37 +0800

    while loop, if else, and print functions

--- recreate_count_function.py
+++ recreate_count_function.py
@@ -1,3 +1,15 @@
 #while loop
 #if else statement
 #print
+def manual_count(text, sub):
+    count = 0
+    sub_len = len(sub)
+    i = 0
+    while i <= len(text) - sub_len:
+        if text[i: i + sub_len] == sub:
+            count += 1
+            i += sub_len
+        else:
+            i += 1
+    return count
+print(manual_count("aaaa", "aa")) 
