d104108ea6e08ee74839b9264045552d773bd246
AmeerOquendo <ameeroquendo29@gmail.com>
2026-04-05 00:42:35 +0800

    if statement, count, and print

--- recreate_zfill_function.py
+++ recreate_zfill_function.py
@@ -1,3 +1,12 @@
 #if statement
 #count
 #print
+def manual_zfill(text, width):
+    if len(text) >= width:
+        return text
+    padding_count = width - len(text)
+    zeros = "0" * padding_count
+    if text[0] in ("+", "-"):
+        return text[0] + zeros + text[1:]
+    return zeros + text
+print(manual_zfill("-7", 4))
