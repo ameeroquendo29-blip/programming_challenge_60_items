d92011547172f863e2658428f33be4b2c50ffda7
AmeerOquendo <ameeroquendo29@gmail.com>
2026-04-05 00:51:12 +0800

    finished the code

--- recreate_index_function.py
+++ recreate_index_function.py
@@ -2,3 +2,10 @@
 #if statement
 #errors
 #print
+def manual_index(text, sub):
+    sub_len = len(sub)
+    for i in range(len(text) - sub_len + 1):
+        if text[i: i + sub_len] == sub:
+            return i
+    raise ValueError(f"substring '{sub}' not found")
+print(manual_index("Snake", "ake"))
